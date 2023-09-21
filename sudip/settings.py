f3adb4fb9b1e122e40dfa284f1eb7d5826a232f6
Rokon Ron <rokon.raz@gmail.com>
2023-09-21 19:22:00 +0600

    Deploying for Commit.

--- sudip/settings.py
+++ sudip/settings.py
@@ -101,15 +101,21 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 JAZZMIN_SETTINGS = {
--- sudip/urls.py
+++ sudip/urls.py
@@ -9,5 +9,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('messageApp.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
