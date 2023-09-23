f5433fc2bd4f71783958388ffa5e1f0821faad13
Rokon Ron <rokon.raz@gmail.com>
2023-09-23 11:50:06 +0600

    Update urls for BlogDetails.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('blogHome/', views.BlogListView.as_view(), name='block_views'),
+    path('blogHome/<int:pk>/', views.BlogDetailsView.as_view(), name='block_Details_Views'),
 ]
