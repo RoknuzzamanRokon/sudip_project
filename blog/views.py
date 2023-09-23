55e30fe2092b734ea3c94ff9df7bd9e90e7d19bf
Rokon Ron <rokon.raz@gmail.com>
2023-09-23 11:47:24 +0600

    Create a new template for blog_details.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from . import models
 
 # Create your views here.
 class BlogListView(ListView):
     model = models.Post
     template_name = 'blog_home.html'
+    
+
+class BlogDetailsView(DetailView):
+    model = models.Post
+    template_name = 'blog_details.html'
+    
+    
