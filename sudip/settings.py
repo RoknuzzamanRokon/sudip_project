cd5aba54f98a5ffadb1183dd8743adee13297030
Rokon Ron <rokon.raz@gmail.com>
2023-09-28 18:18:40 +0600

    Debug fix logout section.

--- sudip/settings.py
+++ sudip/settings.py
@@ -126,14 +126,14 @@ STORAGES = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 JAZZMIN_SETTINGS = {
-    # # title of the window (Will default to current_admin_site.site_title if absent or None)
-    # "site_title": "Library Admin",
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "Ursam.",
     
-    # # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    # "site_header": "Library",
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "Welcome to Ursam.",
     
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_brand": "Ursam",
+    "site_brand": "Ursam.",
 
 }
 
